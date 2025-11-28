e0d76f98acb833a8d8c57c572c45df53746cf5fc
Rosana da Silva <srosanaf@gmail.com>
2025-11-28 15:24:44 -0300

    removendo o .only esquecido

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,16 +1,13 @@
 describe('Login', () => {
   beforeEach(() => {
     cy.visit('http://localhost:4000')
-    cy.screenshot('apos-visitar-pagina')
 })
 
-  it.only('Login com dados válidos deve permitir entrada no sistema', () => {
+  it('Login com dados válidos deve permitir entrada no sistema', () => {
     //Act
     cy.get('#username').click().type('julio.lima')
     cy.get('#senha').click().type('123456')
-    cy.screenshot('apos-preencher-dados-validos')
     cy.contains('button','Entrar').click()
-    cy.screenshot('apos-clicar-no-botao-entrar')
     //Assert
     cy.contains('h4', 'Realizar Transferência').should('be.visible')
 
